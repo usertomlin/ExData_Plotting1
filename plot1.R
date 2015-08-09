beaf5a42ee2b958b7cb06a497d62bca8aac6b0e7
unknown <moderntone@gmail.com>
2015-08-10 00:34:24 +0800

    message

---
+++ plot1.R
@@ -0,0 +1,33 @@
+
+setwd("D:/Online Courses/Data Science/Exploratory Data Analysis")
+library("iterators");
+
+lines <- ireadLines(file.path("household_power_consumption.txt") );
+columnNames = nextElem(lines);
+columnNames = strsplit(columnNames, split = ";")[[1]]
+
+data = data.frame(matrix("", 10000, 9) , stringsAsFactors = FALSE);
+colnames(data) = columnNames;
+
+rowCount = 0;
+for (i in 1: 100000){
+  line = nextElem(lines);
+  elements = strsplit(line, split = ";")[[1]]
+  date = elements[1];
+  if (date == "2/1/2007" | date == "2/2/2007"){
+    rowCount = rowCount + 1;
+    data[rowCount, ] = elements;
+  }
+}
+
+data = data[1:rowCount, ];
+
+################
+
+data$Global_active_power = as.numeric(data$Global_active_power);
+
+
+png("plot1.png", width = 480, height = 480)
+hist(x = data$Global_active_power, col = "red",  xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+
+dev.off()
