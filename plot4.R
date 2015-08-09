d18ea15e49bce61c22163038e1459b8324e06132
unknown <moderntone@gmail.com>
2015-08-10 00:45:33 +0800

    message

--- plot4.R
+++ plot4.R
@@ -33,5 +33,21 @@ data$date_time = as.POSIXct(data$date_time);
 ################
 
 
+png("plot4.png", width = 480, height = 480)
+
+par(mfrow=c(2,2));
+
+plot(Global_active_power ~ date_time, data = data, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "");
+
+plot(Voltage ~ date_time, data = data, type ="l",xlab = "datetime")
+
+plot(Sub_metering_1 ~ date_time, data = data, col = "green",  ylab = "Energy sub metering", xlab = "", ylim = c(0,30)  );
+lines(Sub_metering_2 ~ date_time, data = data, col = "red", xlab = "", ylim = c(0,30));
+lines(Sub_metering_3 ~ date_time, data = data, col = "blue", xlab = "", ylim = c(0,30));
+legend("topright", lty=c(1,1), col = c("green", "red", "blue"), legend = columnNames[7:9])
 
 
+
+plot(Global_reactive_power ~ date_time, data = data, type ="l",xlab = "datetime")
+
+dev.off();
